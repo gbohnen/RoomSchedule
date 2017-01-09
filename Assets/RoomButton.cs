646e1729eae3e067400c71bcdb85cf0455825546
gbohnen <fezmonkey@live.com>
2017-01-09 02:28:08 -0700

    more?

---
+++ Assets/RoomButton.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RoomButton : MonoBehaviour {
+
+    public Text buttonName;
+    public Toggle cleaned;
+    public Text dateCleaned;
+    public Image condition;
+    public Text grade;
+
+    public Color terrible;
+    public Color bad;
+    public Color moderate;
+    public Color good;
+    public Color great;
+
+    public Room thisRoom;
+
+    public void LoadData(Room newRoom)
+    {
+        buttonName.text = newRoom.RoomName;
+
+        cleaned.isOn = (Random.Range(0, 2) == 1);
+
+        int blurp = Random.Range(0, 5);
+        switch (blurp)
+        {
+            case 0: condition.color = terrible; break;
+            case 1: condition.color = bad; break;
+            case 2: condition.color = moderate; break;
+            case 3: condition.color = good; break;
+            case 4: condition.color = great; break;
+        }
+
+        thisRoom = newRoom;
+    }
+
+    public void ClickRoom()
+    {
+
+    }
+}
---
+++ Assets/RoomManager.cs
@@ -0,0 +1,183 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Xml;
+using System.Linq;
+using UnityEngine.UI;
+
+public enum Building { ACAD, Centennial, Columbine, Cucharas, Dwire, Engineering, Osborne, Satellite, UHall }
+
+public class Room
+{
+    // date of last cleaning. loaded from bin file
+    public string DateCleaned
+    { get; set; }
+
+    public Building Building
+    { get; set; }
+
+    // name of the room. loaded from xml
+    public string RoomName
+    { get; set; }
+
+    // list of classes within the room. loaded from xml
+
+    // last grade of room. loaded from private xml
+
+}
+
+public class RoomManager : MonoBehaviour {
+
+    Dictionary<string, Room> roomList;
+
+    public GameObject roomButtonPrefab;
+    public Transform scrollRect;
+    public Scrollbar scrollBar;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        // load default objects
+        roomList = new Dictionary<string, Room>();
+
+
+        // load room names and times
+        TextAsset roomFile = (TextAsset)Resources.Load(@"RoomData");
+        XmlDocument roomDoc = new XmlDocument();
+        roomDoc.LoadXml(roomFile.text);
+
+        XmlNodeList list = roomDoc.GetElementsByTagName("rooms");
+        XmlNodeList buildingList = list[0].ChildNodes;
+
+        // load each room from each building
+        foreach (XmlNode buildingNode in buildingList)
+        {
+            foreach (XmlNode roomNode in buildingNode.ChildNodes)
+            {
+                // build name of room
+                string name;
+                if (buildingNode.Name != "Satellite")
+                    name = buildingNode.Name + " " + roomNode.Name.Substring(1);
+                else
+                    name = roomNode.Name;
+
+                // new room
+                Room room = new Room();
+                room.RoomName = name;
+                room.Building = (Building)Enum.Parse(typeof(Building), buildingNode.Name);
+
+                // load room times
+                // something something parse roomNode.InnerText
+
+                // load date cleaned
+                // something something binary formatter and parser
+
+                // load most recent grade
+                // something something xml parser other document
+
+                // add the room to list
+                roomList.Add(name, room);
+            }
+        }
+
+        List<Room> temp = roomList.Values.ToList<Room>();
+        var linqlist = temp.Where( x => x.RoomName != null).ToList<Room>();
+        BuildRoomList(linqlist);
+	}
+
+    public void BuildRoomList(List<Room> list)
+    {
+        // destroy all buttons 
+        for (int i = scrollRect.childCount - 1; i >= 0; i--)
+        {
+            Destroy(scrollRect.GetChild(i).gameObject);
+        }
+
+        // populate new list
+        foreach (Room room in list)
+        {
+            GameObject button = Instantiate(roomButtonPrefab);
+            button.transform.SetParent(scrollRect);
+
+            button.GetComponent<RoomButton>().LoadData(room);
+        }
+
+        scrollBar.value = 1;
+    }
+
+    #region Navigation Buttons
+
+    public void ClickAll()
+    {
+        List<Room> temp = roomList.Values.ToList<Room>();
+        var linqlist = temp.Where(x => x.RoomName != null).ToList<Room>();
+        BuildRoomList(linqlist);
+    }
+
+    public void ClickACAD()
+    {
+        List<Room> temp = roomList.Values.ToList<Room>();
+        var linqlist = temp.Where(x => x.Building == Building.ACAD).ToList<Room>();
+        BuildRoomList(linqlist);
+    }
+
+    public void ClickCentennial()
+    {
+        List<Room> temp = roomList.Values.ToList<Room>();
+        var linqlist = temp.Where(x => x.Building == Building.Centennial).ToList<Room>();
+        BuildRoomList(linqlist);
+    }
+
+    public void ClickColumbine()
+    {
+        List<Room> temp = roomList.Values.ToList<Room>();
+        var linqlist = temp.Where(x => x.Building == Building.Columbine).ToList<Room>();
+        BuildRoomList(linqlist);
+    }
+
+    public void ClickCucharas()
+    {
+        List<Room> temp = roomList.Values.ToList<Room>();
+        var linqlist = temp.Where(x => x.Building == Building.Cucharas).ToList<Room>();
+        BuildRoomList(linqlist);
+    }
+
+    public void ClickDwire()
+    {
+        List<Room> temp = roomList.Values.ToList<Room>();
+        var linqlist = temp.Where(x => x.Building == Building.Dwire).ToList<Room>();
+        BuildRoomList(linqlist);
+    }
+
+    public void ClickEngineering()
+    {
+        List<Room> temp = roomList.Values.ToList<Room>();
+        var linqlist = temp.Where(x => x.Building == Building.Engineering).ToList<Room>();
+        BuildRoomList(linqlist);
+    }
+
+    public void ClickOsborne()
+    {
+        List<Room> temp = roomList.Values.ToList<Room>();
+        var linqlist = temp.Where(x => x.Building == Building.Osborne).ToList<Room>();
+        BuildRoomList(linqlist);
+    }
+
+    public void ClickSatellite()
+    {
+        List<Room> temp = roomList.Values.ToList<Room>();
+        var linqlist = temp.Where(x => x.Building == Building.Satellite).ToList<Room>();
+        BuildRoomList(linqlist);
+    }
+
+    public void ClickUHall()
+    {
+        List<Room> temp = roomList.Values.ToList<Room>();
+        var linqlist = temp.Where(x => x.Building == Building.UHall).ToList<Room>();
+        BuildRoomList(linqlist);
+    }
+
+    #endregion
+}
